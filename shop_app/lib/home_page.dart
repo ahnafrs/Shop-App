217065912ed9d7f285e8b5d48d34b09bda1e1d1e
Ahnaf <ahnafrahman71@gmail.com>
2023-09-21 16:27:35 +0600

    BottomNavigationBar Ui and Function done

--- shop_app/lib/home_page.dart
+++ shop_app/lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_app/cart_page.dart';
 import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/product_list.dart';
 import 'package:shop_app/products_card.dart';
 import 'package:shop_app/products_details_page.dart';
 
@@ -12,125 +14,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> tagList = ["All", "Adidas", "Nike", "Puma", "Bata"];
-
   var selected = 0;
+  int currentPage = 0;
+  List<Widget> pages = [
+    ProductList(),
+    CartPage(),
+  ];
   @override
   Widget build(BuildContext context) {
-    final border = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.grey,
-      ),
-      borderRadius: BorderRadius.horizontal(
-        left: Radius.circular(50),
-      ),
-    );
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "Shoes\nCollection",
-                    style: GoogleFonts.lato(
-                      textStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32,
-                        height: 1,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search',
-                      hintStyle: TextStyle(
-                          fontWeight: FontWeight.normal, fontSize: 15),
-                      prefixIcon: Icon(Icons.search),
-                      border: border,
-                      enabledBorder: border,
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            // Padding(
-            //   padding: const EdgeInsets.all(8.0),
-            //   child: Container(height: 40, child: TagList()),
-            // ),
-            SizedBox(
-              height: 120,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selected = index;
-                      });
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(4),
-                      child: Chip(
-                        label: Text(tagList[index]),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        side: BorderSide(
-                          color: selected == index
-                              ? Color.fromARGB(255, 250, 202, 29)
-                              : Color.fromARGB(255, 233, 243, 248),
-                        ),
-                        backgroundColor: selected == index
-                            ? Color.fromARGB(255, 250, 202, 29)
-                            : Color.fromARGB(255, 233, 243, 248),
-                      ),
-                    ),
-                  );
-                },
-                separatorBuilder: (_, index) => SizedBox(
-                  width: 10,
-                ),
-                itemCount: tagList.length,
-              ),
-            ),
-            Expanded(
-              child: ListView.separated(
-                  itemBuilder: (context, index) {
-                    final product = products[index];
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          Navigator.of(context)
-                              .push(MaterialPageRoute(builder: (context) {
-                            return ProductsDetailsPage(product: product);
-                          }));
-                        });
-                      },
-                      child: ProductsCard(
-                        title: product['title'] as String,
-                        price: product['price'] as double,
-                        image: product['imageUrl'] as String,
-                        backgroundColor: index.isEven
-                            ? Color.fromARGB(255, 165, 208, 228)
-                            : Color.fromARGB(255, 233, 243, 248),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (_, index) => SizedBox(
-                        height: 10,
-                      ),
-                  itemCount: products.length),
-            )
-          ],
-        ),
+      body: IndexedStack(
+        index: currentPage,
+        children: pages,
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Color.fromARGB(255, 250, 202, 29),
+          iconSize: 35,
+          selectedFontSize: 0,
+          unselectedFontSize: 0,
+          onTap: (value) {
+            setState(() {
+              currentPage = value;
+            });
+          },
+          currentIndex: currentPage,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: '',
+            ),
+          ]),
     );
   }
 }
---
+++ shop_app/lib/product_list.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/products_card.dart';
+import 'package:shop_app/products_details_page.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  final List<String> tagList = ["All", "Adidas", "Nike", "Puma", "Bata"];
+
+  var selected = 0;
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey,
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  "Shoes\nCollection",
+                  style: GoogleFonts.lato(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
+                      height: 1,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    hintStyle:
+                        TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
+                    prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Container(height: 40, child: TagList()),
+          // ),
+          SizedBox(
+            height: 120,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selected = index;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Chip(
+                      label: Text(tagList[index]),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      side: BorderSide(
+                        color: selected == index
+                            ? Color.fromARGB(255, 250, 202, 29)
+                            : Color.fromARGB(255, 233, 243, 248),
+                      ),
+                      backgroundColor: selected == index
+                          ? Color.fromARGB(255, 250, 202, 29)
+                          : Color.fromARGB(255, 233, 243, 248),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (_, index) => SizedBox(
+                width: 10,
+              ),
+              itemCount: tagList.length,
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return ProductsDetailsPage(product: product);
+                        }));
+                      });
+                    },
+                    child: ProductsCard(
+                      title: product['title'] as String,
+                      price: product['price'] as double,
+                      image: product['imageUrl'] as String,
+                      backgroundColor: index.isEven
+                          ? Color.fromARGB(255, 165, 208, 228)
+                          : Color.fromARGB(255, 233, 243, 248),
+                    ),
+                  );
+                },
+                separatorBuilder: (_, index) => SizedBox(
+                      height: 10,
+                    ),
+                itemCount: products.length),
+          )
+        ],
+      ),
+    );
+  }
+}
