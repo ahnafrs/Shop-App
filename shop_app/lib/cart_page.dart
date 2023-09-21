03528f6c38fa2dfe8c312d152dc15d0b4db8a6a6
Ahnaf <ahnafrahman71@gmail.com>
2023-09-21 16:48:48 +0600

    Cart Page ui Done

--- shop_app/lib/cart_page.dart
+++ shop_app/lib/cart_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context, index) {
+            final cartItem = cart[index];
+            return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+                  radius: 45,
+                ),
+                title: Text(cartItem['title'] as String),
+                subtitle: Text('Size: ${cartItem['sizes']}'),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    )));
+          }),
+    );
   }
 }
--- shop_app/lib/global_variables.dart
+++ shop_app/lib/global_variables.dart
@@ -32,3 +32,13 @@ final products = [
     'sizes': [8, 9, 10],
   },
 ];
+final cart = [
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': 10,
+  }
+];
